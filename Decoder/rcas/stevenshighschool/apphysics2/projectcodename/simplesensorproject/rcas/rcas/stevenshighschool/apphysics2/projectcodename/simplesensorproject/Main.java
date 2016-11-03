ca63d0eff283b72fd45c82ef961935e3cf1582c5
AlanZhuTheAsian <alanyzhu@gmail.com>
2016-11-03 15:30:15 -0600

    Added csv conversion

--- Decoder/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/rcas/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/Main.java
+++ Decoder/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/rcas/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/Main.java
@@ -1,7 +1,9 @@
 package rcas.stevenshighschool.apphysics2.projectcodename.simplesensorproject;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import rcas.stevenshighschool.apphysics2.projectcodename.simplesensorproject.*;
 import java.util.Date;
@@ -24,11 +26,41 @@ public class Main {
             c.printStackTrace();
             return;
         }
-        for(DataPoint pnt: dataPoint){
-            System.out.println(pnt.time);
-            System.out.println("A_X: "+pnt.a_x+", A_Y: "+pnt.a_y+", A_Z:"+pnt.a_z +", P: "+pnt.p);
-            System.out.println("lat: "+pnt.lat+", lon: "+pnt.lon+", alt:"+pnt.alt);
-            System.out.println("====");
+        try {
+            PrintWriter pw = new PrintWriter(new File("data.csv"));
+            StringBuilder sb = new StringBuilder();
+            sb.append("time");
+            sb.append(',');
+            sb.append("a_x");
+            sb.append(',');
+            sb.append("a_y");
+            sb.append(',');
+            sb.append("a_z");
+            sb.append(',');
+            sb.append("p");
+            sb.append('\n');
+
+
+            for (DataPoint pnt : dataPoint) {
+                System.out.println(pnt.time);
+                System.out.println("A_X: " + pnt.a_x + ", A_Y: " + pnt.a_y + ", A_Z:" + pnt.a_z + ", P: " + pnt.p);
+                System.out.println("lat: " + pnt.lat + ", lon: " + pnt.lon + ", alt:" + pnt.alt);
+                System.out.println("====");
+                sb.append(pnt.time);
+                sb.append(',');
+                sb.append(pnt.a_x);
+                sb.append(',');
+                sb.append(pnt.a_y);
+                sb.append(',');
+                sb.append(pnt.a_z);
+                sb.append(',');
+                sb.append(pnt.p);
+                sb.append('\n');
+            }
+            pw.write(sb.toString());
+            pw.close();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
