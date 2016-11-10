e7968d37479fbe22186493a143f6b03d2f1c8828
AlanZhuTheAsian <alanyzhu@gmail.com>
2016-11-10 08:54:48 -0700

    Updated Decoder; Created Animations!

--- Decoder/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/rcas/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/DataPoint.java
+++ Decoder/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/rcas/rcas/stevenshighschool/apphysics2/projectcodename/simplesensorproject/DataPoint.java
@@ -2,12 +2,10 @@ package rcas.stevenshighschool.apphysics2.projectcodename.simplesensorproject;
 
 import java.util.Date;
 
-/**
- * Class that will be logged as a separate file
- * Excerpted straight from the Android project
- */
 
-public class DataPoint implements java.io.Serializable{
+class DataPoint implements java.io.Serializable{
+    //things in a data point
+    public float t;
     public float a_y;
     public float a_x;
     public float a_z;
@@ -16,13 +14,48 @@ public class DataPoint implements java.io.Serializable{
     public double lon;
     public double alt;
     public Date time;
+    public float mag_x;
+    public float mag_y;
+    public float mag_z;
+    public float rh;
+    public float rot_x;
+    public float rot_y;
+    public float rot_z;
+    public float g_x;
+    public float g_y;
+    public float g_z;
+    //private float ext_p;
+    //private float ext_t;
+    //private float ard_alt;
+
+
+
+    //allows for this to be used in our decoder--it's a specific value
     private static final long serialVersionUID= 862329458582895689L;
 
-    public DataPoint(float a_x, float a_y, float a_z, float p, Date time){
-        this.a_y=a_y;
+    //initializer
+    public DataPoint(float t, float g_x, float g_y, float g_z, float rot_x, float rot_y, float rot_z, float rh, float mag_x, float mag_y, float mag_z, float a_x, float a_y, float a_z, float p, Date time){
+        this.t = t;
+        this.g_x = g_x;
+        this.g_y = g_y;
+        this.g_z = g_z;
+        this.rot_x = rot_x;
+        this.rot_y = rot_y;
+        this.rot_z = rot_z;
+        this.rh = rh;
+        this.a_y = a_y;
         this.a_x=a_x;
         this.a_z=a_z;
+        this.mag_x=mag_x;
+        this.mag_y=mag_y;
+        this.mag_z=mag_z;
         this.p=p;
         this.time=time;
+        this.lat=-1;
+        this.lon=-1;
+        this.alt=-1;
+        //this.ext_p = ext_p;
+        //this.ext_t = ext_t;
+        //this.ard_alt = ard_alt;
     }
 }
