87fb6d427ecf4207c71ffc9837f2864147340358
AZtheAsian <alanzhutesting@gmail.com>
2017-06-06 17:05:33 -0600

    Update serial test.py

--- Arduino code/python-for-testing/serial test.py
+++ Arduino code/python-for-testing/serial test.py
@@ -1,6 +1,9 @@
 import serial
 import time
 
+"""
+This is just a class that (pretty badly) re-implements Serial the Monitor in Arduino IDE. (For some reason).
+"""
 ser=serial.Serial('COM8', 9600, timeout=0)
 ser.write(b"test");
 while True:
